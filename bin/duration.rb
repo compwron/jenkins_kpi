5088f8224c5ef51a4ce40c9cd18f3f10e931bd1e
Linda Goldstein <lgoldste@thoughtworks.com>
2012-02-27 17:18:05 -0800

    removed hardcoded url from bin

--- bin/duration.rb
+++ bin/duration.rb
@@ -2,4 +2,4 @@
 
 require File.expand_path(File.dirname(__FILE__) + '/../lib/pipeline')
 
-puts "#{Pipeline.new.duration("http://localhost:8080/job/job1.in.pipeline/")} minutes"
+puts "#{Pipeline.new.duration(ARGV[0])} minutes"
